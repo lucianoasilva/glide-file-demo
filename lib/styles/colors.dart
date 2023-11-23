ada81c76423f43f50b17d13a5b6cc4c9c270e5b6
Luciano Silva <luciano.adlsilva@gmail.com>
2023-11-23 16:46:17 -0300

    colores básicos agregados

---
+++ lib/styles/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Colors.orangeAccent;
+const Color primaryColorLight = Color.fromARGB(150, 255, 184, 95);
+const Color secondaryColor = Color.fromARGB(255, 24, 24, 24);
+const Color tertiaryColor = Color.fromARGB(255, 27, 27, 27);
+const Color dividerColor = Color.fromARGB(255, 66, 66, 66);
+const Color fontColor1 = Colors.white;
