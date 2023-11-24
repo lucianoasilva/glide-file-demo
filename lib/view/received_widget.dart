30b5fef4a711cacf2f3b80b1ea4c2f387b0a152a
Luciano Silva <luciano.adlsilva@gmail.com>
2023-11-24 16:38:30 -0300

    edición front de Received Widget

--- lib/view/received_widget.dart
+++ lib/view/received_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../styles/colors.dart';
+
 class ReceivedWidget extends StatefulWidget {
   const ReceivedWidget({super.key});
 
@@ -10,6 +12,58 @@ class ReceivedWidget extends StatefulWidget {
 class _ReceivedWidgetState extends State<ReceivedWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      color: Colors.green,
+      child: Column(children: <Widget>[
+        const Expanded(
+          child: Align(
+            alignment: Alignment(0, 0),
+            child: Text(
+              "¡Recibido!",
+              style: TextStyle(
+                fontFamily: 'Mukta',
+                fontWeight: FontWeight.bold,
+                color: fontColor1,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
+        const Expanded(
+          child: Align(
+              alignment: Alignment(0, -2),
+              child: Icon(
+                Icons.check_rounded,
+                color: fontColor1,
+                size: 220,
+              )),
+        ),
+        Expanded(
+          child: Align(
+            alignment: const Alignment(0, 0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(180, 50),
+                  backgroundColor: fontColor1,
+                  elevation: 0,
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
+                      ))),
+              onPressed: () {},
+              child: const Text(
+                "Abrir archivo",
+                style: TextStyle(
+                  fontFamily: 'Mukta',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
