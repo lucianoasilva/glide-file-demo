9442026e84dee247237def3d7180c8c56b4ce95d
Luciano Silva <luciano.adlsilva@gmail.com>
2023-11-28 16:00:31 -0300

    edición front de Sending Widget

--- lib/view/sending_widget.dart
+++ lib/view/sending_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../styles/colors.dart';
+
 class SendingWidget extends StatefulWidget {
   const SendingWidget({super.key});
 
@@ -8,8 +10,83 @@ class SendingWidget extends StatefulWidget {
 }
 
 class _SendingWidgetState extends State<SendingWidget> {
+
+  String currentState = "initState";
+
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _connect();
+    });
+  }
+
+  void _connect() {
+    currentState = "Conectando...";
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        _sending();
+      });
+    });
+  }
+
+  void _sending() {
+    currentState = "Enviando...";
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        currentState = "ENVIADO!";
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+        color: secondaryColor,
+        child: Column(children: <Widget>[
+          const Expanded(
+            child: Align(
+              alignment: Alignment(0, 0.5),
+              child: Text(
+                "Enviando...",
+                style: TextStyle(
+                  fontFamily: 'Mukta',
+                  fontWeight: FontWeight.bold,
+                  color: fontColor1,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Align(
+              child: SizedBox(
+                width: 200.0,
+                height: 200.0,
+                child: CircularProgressIndicator(
+                  color: primaryColor,
+                  backgroundColor: primaryColorLight,
+                  strokeWidth: 15.0,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: Align(
+              alignment: const Alignment(0, -0.5),
+              child: Text(
+                currentState,
+                style: const TextStyle(
+                  fontFamily: 'Mukta',
+                  fontWeight: FontWeight.w100,
+                  color: fontColor1,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          )
+        ]));
   }
 }
