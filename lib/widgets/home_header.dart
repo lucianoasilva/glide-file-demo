5a2dc538f5e232fa8ddf26e17d18a975910234a4
Luciano Silva <luciano.adlsilva@gmail.com>
2023-11-28 11:44:49 -0300

    indicador 'Front Demo' agregado en Home Header

--- lib/widgets/home_header.dart
+++ lib/widgets/home_header.dart
@@ -29,16 +29,39 @@ class _HomeHeaderState extends State<HomeHeader> {
                     child: Image.asset('resources/icons/glide-file_logo.png',
                         color: primaryColor),
                   ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Glide-File',
-                    style: TextStyle(
-                      fontFamily: 'LobsterTwo',
-                      color: fontColor1,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 50,
-                    ),
-                  )
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Glide-File',
+                        style: TextStyle(
+                          fontFamily: 'LobsterTwo',
+                          color: fontColor1,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 50,
+                        ),
+                      ),
+                      const SizedBox(width: 3),
+                      Container(
+                        padding: const EdgeInsets.all(2.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6.0),
+                          color: primaryColor, // Puedes cambiar el color del rectángulo según tus preferencias
+                        ),
+                        child: const Text(
+                          'Front Demo',
+                          style: TextStyle(
+                            fontFamily: 'Mukta',
+                            color: fontColor1,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 32),
+                    ],
+                  ),
                 ],
               ),
               Row(
