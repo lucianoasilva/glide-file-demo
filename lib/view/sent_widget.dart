ec86176a33a505968099e46f755c48a1d9bf79d8
Luciano Silva <luciano.adlsilva@gmail.com>
2023-11-28 11:56:09 -0300

    edición front de Sent Widget

--- lib/view/sent_widget.dart
+++ lib/view/sent_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../styles/colors.dart';
+
 class SentWidget extends StatefulWidget {
   const SentWidget({super.key});
 
@@ -9,7 +11,42 @@ class SentWidget extends StatefulWidget {
 
 class _SentWidgetState extends State<SentWidget> {
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pop(context);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Material(
+      color: Colors.green,
+      child: Column(children: <Widget>[
+        Expanded(
+          child: Align(
+            alignment: Alignment(0, 0),
+            child: Text(
+              "¡Enviado!",
+              style: TextStyle(
+                fontFamily: 'Mukta',
+                fontWeight: FontWeight.bold,
+                color: fontColor1,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Align(
+              alignment: Alignment(0, -2),
+              child: Icon(
+                Icons.check_rounded,
+                color: fontColor1,
+                size: 220,
+              )),
+        ),
+      ]),
+    );
   }
 }
