801795aba203523ace06c8a58338d1f3e43081b6
Luciano Silva <luciano.adlsilva@gmail.com>
2023-12-14 18:19:17 -0300

    splash screen agregada

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rive_splash_screen/rive_splash_screen.dart';
 
+import '../styles/colors.dart';
 import '../services/notification_services.dart';
 import '../controller/requirement_state_controller.dart';
 import '../view/home_widget.dart';
@@ -19,11 +21,18 @@ class MyApp extends StatelessWidget {
     Get.put(RequirementStateController());
 
     return MaterialApp(
-        title: 'Glide-File Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage());
+      title: 'Glide-File Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen.navigate(
+        name: 'resources/assets/splash.riv',
+        next: (context) => const HomePage(),
+        until: () => Future.delayed(const Duration(seconds: 3)),
+        startAnimation: 'Splash',
+        backgroundColor: secondaryColor,
+      ),
+    );
   }
 }
